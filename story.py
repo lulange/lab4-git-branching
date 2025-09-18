fc79342c2e0a3ae71ed435aa10354c29fa9022db
Lucas Lange <53273617+lulange@users.noreply.github.com>
2025-09-18 13:29:28 -0400

    added options on the hero path to fight or run from the dragon

--- story.py
+++ story.py
@@ -16,7 +16,21 @@ def left_path():
     if choice == "pull it":
         print("The sword comes free and as you marvel at its eerie blue glow, you hear an ear-splitting wooshing sound.")
         print("A dragon suddenly lands in front of you. It appears not to be pleased you have taken the sword.")
-    
+    elif choice == "leave it":
+        print("You walk around for a while but ultimately can't find your way out of the forest.")
+        return
+    else:
+        return
+    choice = input("Thoughts racing, you realize you can either run or fight. Which do you choose? (run/fight)")
+    if choice == "run":
+        print("You dash in the other direction right before the dragon pounces.")
+        print("The sword glows brightly as the dragon gets close with a snap of its jaws.")
+    elif choice == "fight":
+        print("You brandish the sword. It glows brighter as if to say it was made for this")
+        print("The dragon pounces on you, engulfing you in its mouth.")
+        print("But since you held the sword up, the dragon impales itself on the bright steel.")
+    else:
+        print("You stand still and are eaten.")
 
 
 def right_path():
