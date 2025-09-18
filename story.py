c31440f3ea79650af9f77d50ba39866c4485a200
Lucas Lange <53273617+lulange@users.noreply.github.com>
2025-09-18 17:45:25 +0000

    you can now defeat the squirrel and choose to be a villain or not

--- story.py
+++ story.py
@@ -20,6 +20,17 @@ def right_path():
     choice = input("which do you choose? (fight/say sorry)")
     if choice == "fight":
         print("You accept the squirel's duel")
+        print("The squirrel produces a small sword from behind a log.")
+        print("You draw your daggers.")
+        print("Before the duel can properly begin, you quickly throw a dagger, shaving off a whisker from the squirrel's face.")
+        print("The squirrel runs away in fright.")
+        stomp = input("You are now free to stomp all over and make sure to crush the squirrel's friend's home. (stomp/leave)")
+        if stomp == "stomp":
+            print("You ruthlessly stomp all over.")
+        elif stomp == "leave":
+            print("You walk away glad you didn't get scratched up.")
+        else:
+            return
     elif choice == "say sorry":
         print("You apologize and the squirrel realizes you meant nothing by it.")
     else:
